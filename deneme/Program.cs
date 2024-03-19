57bc62c8e8e920c727a2c2b3956649b89ef56381
karakoc-ali <ali.yazilimci@gmail.com>
2024-03-19 12:57:30 +0300

    proje oluşturuldu

---
+++ deneme/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
