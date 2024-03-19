d02a9e3832c66c728aee8edec9312d327ce6f508
karakoc-ali <163824156+karakoc-ali@users.noreply.github.com>
2024-03-19 14:53:14 +0300

    Update Program.cs

--- deneme/Program.cs
+++ deneme/Program.cs
@@ -5,7 +5,7 @@ namespace deneme
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   //dğişiklik nerede
             Console.WriteLine("Hello World!");
         }
     }
